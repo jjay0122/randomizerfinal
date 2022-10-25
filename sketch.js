38a45bd10bb5cf5b08507882914acd95317aa19d
Jay <73442867+jjay0122@users.noreply.github.com>
2022-10-25 06:25:54 -0700

    yes

--- sketch.js
+++ sketch.js
@@ -1,39 +1,45 @@
 //pick your poison that will give supernatural abilities
-let poison = [{
-  name:"Dragon Blood", 
-  color: "blood red",
-  power: "Fire Manipulation"
-}, {
-  name:"Unicorn Horn", 
-  color: "rose pink",
-  power: "Color Manipulation"
-}, {
-  name:"Angel Feather", 
-  color: "cloud white",
-  power: "Light Manipulation"
-}, {
-  name:"Demon Scar", 
-  color: "sun yellow",
-  power: "Necromancy"
-}, {
-  name:"Fairy Magic", 
-  color:"sky blue",
-  power: "Nature Manipulation"
-}, {
-  name:"Rainbow Gold", 
-  color:"golden brown",
-  power: "Shapeshifting"
-}];
+let poison = [
+  {
+    name: "Dragon Blood",
+    color: "blood red",
+    power: "Fire Manipulation",
+  },
+  {
+    name: "Unicorn Horn",
+    color: "rose pink",
+    power: "Color Manipulation",
+  },
+  {
+    name: "Angel Feather",
+    color: "cloud white",
+    power: "Light Manipulation",
+  },
+  {
+    name: "Demon Scar",
+    color: "sun yellow",
+    power: "Necromancy",
+  },
+  {
+    name: "Fairy Magic",
+    color: "sky blue",
+    power: "Nature Manipulation",
+  },
+  {
+    name: "Rainbow Gold",
+    color: "golden brown",
+    power: "Shapeshifting",
+  },
+];
 
 let randomIndex;
 let animating = false;
-let powers = [];
+let power = [];
+let imageCounter = 0
 
-function preload(){
-  
-  for (let i = 0; i <= 15; i++){
-    powers[i] = loadImage("assets/power_" + i + ".png");
-    
+function preload() {
+  for (let i = 0; i <= 15; i++) {
+    power[i] = loadImage(`images/power_ ${i}.jpg`);
   }
 }
 
@@ -41,33 +47,35 @@ function setup() {
   createCanvas(500, 500);
   background(220);
   textSize(20);
-  
+  imageMode(CENTER);
+
   text("Click to Order", 190, 50);
   text("Your Poison", 198, 350);
-  
-  console.log(powers);
+
+  console.log(power);
 }
 
 function draw() {
-  
-  if(animating == true){
-   image(power[1], 0, 0);
-    
+  if (animating == true) {
+    clear();
+    image(power[imageCounter], width / 2, height / 2);
+    imageCounter++;
   }
-
 }
 
-function randomizer(){
-  
-  animating = false
-  
-  if (poison[0]){
-    
-  background(random(320, 100));
-  randomIndex=int(random(poison.length));
-    text(`${poison[randomIndex].name}'s color is ${poison[randomIndex].color} 
-it gives the power of ${poison[randomIndex].power}`, 75, 50)
-  poison.splice(randomIndex, 1);
+function randomizer() {
+  animating = false;
+
+  if (poison[0]) {
+    background(random(320, 100));
+    randomIndex = int(random(poison.length));
+    text(
+      `${poison[randomIndex].name}'s color is ${poison[randomIndex].color} 
+it gives the power of ${poison[randomIndex].power}`,
+      75,
+      50
+    );
+    poison.splice(randomIndex, 1);
   } else {
     background(random(320, 100));
     text("Out Of Stock", 195, 200);
@@ -77,5 +85,4 @@ it gives the power of ${poison[randomIndex].power}`, 75, 50)
 function mousePressed() {
   animating = true;
   setTimeout(randomizer, 2000);
-  
 }
